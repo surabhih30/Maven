ef4d64a83b77d66c4d362af517d440358b565008
surabhih30 <surabhihosur@gmail.com>
2022-01-23 23:38:44 +0530

    maven

---
+++ src/test/java/maven/maven/TestMain.java
@@ -0,0 +1,14 @@
+package maven.maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestMain {
+ 
+ @Test
+public void testInputIsEven(){
+ assertTrue(Main.checkIfInputIsAnEvenNumber(23)); // 
+
+ }
+ }
